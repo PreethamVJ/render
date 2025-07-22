13c5e609812463b95337f82167b1c813d7b100e2
PreethamVJ <preethamvj2005@gmail.com>
2025-07-22 20:51:45 +0530

    fixed the file name

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const Note = require('./models/note'); 
+const Note = require('./models/mongo'); 
 app.use(cors());
 app.use(express.json());
 app.use(express.static('dist'));
