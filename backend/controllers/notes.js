c5f0aee970beef40121f4bd235107cb9c51eca3d
PreethamVJ <preethamvj2005@gmail.com>
2025-08-20 22:15:52 +0530

    uibuild

--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -1,15 +1,26 @@
 const jwt = require('jsonwebtoken')
 const notesRouter = require('express').Router()
 const Note = require('../models/mongo')
+const User = require('../models/user')
 const middleware = require('../utils/middleware')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 notesRouter.get('/', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
+  Note.find({})
+    .then(notes => {
+      response.json(notes)
+    })
 })
 
 notesRouter.get('/:id', (request, response, next) => {
-  console.log('GET /notes/:id', request.params.id);
+  console.log('GET /notes/:id', request.params.id)
   Note.findById(request.params.id)
     .then(note => {
       if (note) {
@@ -21,19 +32,32 @@ notesRouter.get('/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-notesRouter.post('/', (request, response, next) => {
-  const body = request.body
+notesRouter.post('/', async (request, response, next) => {
+  try {
+    const body = request.body
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
 
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-  })
+    const user = await User.findById(decodedToken.id)
 
-  note.save()
-    .then(savedNote => {
-      response.json(savedNote)
+    const note = new Note({
+      content: body.content,
+      important: body.important || false,
+      user: user._id,
     })
-    .catch(error => next(error))
+
+    const savedNote = await note.save()
+    user.notes = user.notes.concat(savedNote._id)
+    await user.save()
+
+    response.status(201).json(savedNote)
+  } catch (error) {
+    next(error)
+  }
 })
 
 notesRouter.delete('/:id', (request, response, next) => {
@@ -56,7 +80,7 @@ notesRouter.put('/:id', (request, response, next) => {
       note.content = content
       note.important = important
 
-      return note.save().then((updatedNote) => {
+      return note.save().then(updatedNote => {
         response.json(updatedNote)
       })
     })
